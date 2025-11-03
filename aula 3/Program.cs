baea9d7a985ee6cadf4095fea218144b475425e4
MELYSSA EZEQUIEL SANTOS OLIVEIRA <melyssa.esoliveira@senacsp.edu.br>
2025-11-03 20:42:21 -0300

    primeiro commit com codigo

---
+++ aula 3/Program.cs
@@ -0,0 +1,49 @@
+﻿/*
+Console.WriteLine("informe um numero inteiro");
+string numero1 = Console.ReadLine();
+Console.WriteLine("informe outro numero inteiro");
+string numero2 = Console.ReadLine();*/
+/*
+Console.WriteLine("escreva seu primeiro numero");
+string numero1 = Console.ReadLine();
+Console.WriteLine("escreva seu segundo numero");
+string numero2 = Console.ReadLine();
+int num1 =int.Parse(numero1);
+int num2 =int.Parse(numero2);   
+int soma = num1 + num2;
+Console.WriteLine(" o resultado da sua soma é " + soma);*/
+
+/*
+Console.WriteLine("escreva seu primeiro numero");
+string numero1 = Console.ReadLine();
+Console.WriteLine("escreva seu segundo numero");
+string numero2 = Console.ReadLine();
+int num1 =int.Parse(numero1);
+int num2 =int.Parse(numero2);   
+int soma = num1 - num2;
+Console.WriteLine(" o resultado da sua subtração é"+ soma);*/
+
+/*
+Console.WriteLine("escreva seu primeiro numero");
+string numero1 = Console.ReadLine();
+Console.WriteLine("escreva seu segundo numero");
+string numero2 = Console.ReadLine();
+int num1 =int.Parse(numero1);
+int num2 =int.Parse(numero2);   
+int soma = num1 * num2;
+Console.WriteLine(" o resultado da sua multiplicação é"+ soma);*/
+
+/*
+Console.WriteLine("escreva seu primeiro numero");
+string numero1 = Console.ReadLine();
+Console.WriteLine("escreva seu segundo numero");
+string numero2 = Console.ReadLine();
+int num1 = int.Parse(numero1);
+int num2 = int.Parse(numero2);
+int soma = num1/num2;
+Console.WriteLine(" o resultado da sua divisao é " + soma);*/
+
+
+
+
+
