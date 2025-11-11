910cd75748030f0a6baa4d45905762e97ba00a22
MELYSSA EZEQUIEL SANTOS OLIVEIRA <melyssa.esoliveira@senacsp.edu.br>
2025-11-10 21:53:34 -0300

    conteudo aula 6

---
+++ RevisaoAula5/Program.cs
@@ -0,0 +1,43 @@
+﻿
+int n1;
+int n2;
+
+
+/* Escrevi uma mensagem no console
+Console.WriteLine(" Insira um numero inteiro");
+// Li o conteudo de uma linha, quando o usuario apertou a tecla ENTER
+n1=int.Parse(Console.ReadLine());
+// Escrevi outra mensagem no Console
+
+Console.WriteLine(" insira mais um numero inteiro");
+// novamente li o conteudo da linha, quando o usuario apertou a tecla ENTER
+n2=int.Parse(Console.ReadLine());
+
+// Mostar dois numeros no Console 
+Console.WriteLine("Voce digitou " + n1 +  " e "  + n2);
+
+int soma = n1 + n2;
+Console.WriteLine("Resultado da sua adicao é " + soma);*/
+
+
+Console.WriteLine(" Insira um numero inteiro");
+n1 = int.Parse(Console.ReadLine());
+Console.WriteLine(" insira mais um numero inteiro");
+n2 = int.Parse(Console.ReadLine());
+int soma = n1 - n2;
+Console.WriteLine("Resultado da sua subtracao é " + soma );
+
+/*
+Console.WriteLine(" Insira um numero inteiro");
+n1 = int.Parse(Console.ReadLine());
+Console.WriteLine(" insira mais um numero inteiro");
+n2 = int.Parse(Console.ReadLine());
+int soma = n1 * n2;
+Console.WriteLine("Resultado da sua multiplicao é " + soma);*/
+
+/*Console.WriteLine(" Insira um numero inteiro");
+n1 = int.Parse(Console.ReadLine());
+Console.WriteLine(" insira mais um numero inteiro");
+n2 = int.Parse(Console.ReadLine());
+int soma = n1 / n2;
+Console.WriteLine("Resultado da sua divisao é " + soma); */
---
+++ aula 6/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.Design;
+
+Console.WriteLine(" Qual a sua idade ?");
+
+string resposta = Console.ReadLine();
+const int DEZOITO = 18;
+const int ZERO = 0;
+int idade = int.Parse(resposta);
+bool ehvalido = int . TryParse(resposta, out idade);
+
+if (ehvalido)
+
+    if (idade >= 18)
+    {
+
+        Console.WriteLine(" Usuario é maior de idade ");
+       
+    }
+    else
+    {
+        Console.WriteLine("usuario é menor de idade");
+
+
+    }
+    else 
+    Console.WriteLine( "Valor informado não corresponde");
