0330ea9da55ba311ed5e167d17bf3d05b7cf64c7
MELYSSA EZEQUIEL SANTOS OLIVEIRA <melyssa.esoliveira@senacsp.edu.br>
2025-11-19 20:51:52 -0300

    aula 10

---
+++ aula 10/Program.cs
@@ -0,0 +1,65 @@
+﻿const string AMARELO = "AMARELO";
+const string AZUL = "AZUL";
+const string VERDE = "VERDE";
+const string MSG_VOTO = "Qual é o seu voto? (AMARELO, AZUL ou VERDE)";
+string voto;
+int votosAmarelo = 0;
+int votosVerde = 0;
+int votosAzul = 0;
+int abs = 0;
+for (int cont = 0; cont < 5; cont++)
+{
+    Console.WriteLine(MSG_VOTO);
+    voto = Console.ReadLine();
+    // Apuração
+    switch (voto)
+    {
+        case AMARELO:
+            votosAmarelo++;
+            break;
+        case AZUL:
+            votosAzul++;
+            break;
+        case VERDE:
+            votosVerde++;
+            break;
+        default:
+            abs++;
+            break;
+
+    }
+}
+
+int vencedor = 0;
+if (vencedor < votosAmarelo)
+{
+    vencedor = votosAmarelo;
+}
+if (vencedor < votosAzul)
+{
+    vencedor = votosAzul;
+}
+if (vencedor < votosVerde)
+{
+    vencedor = votosVerde;
+}
+if (vencedor < abs)
+{
+    vencedor = abs;
+}
+if (vencedor == votosAmarelo)
+{
+    Console.WriteLine("O Amarelo é o vencedor.");
+}
+if (vencedor == votosAzul)
+{
+    Console.WriteLine("O Azul é o vencedor.");
+}
+if (vencedor == votosVerde)
+{
+    Console.WriteLine("O Verde é o vencedor.");
+}
+if (vencedor == abs)
+{
+    Console.WriteLine("Não há vencedor dado o número de abstenções.");
+}
